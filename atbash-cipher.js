b3e1e35ce864f2d133e8a6c8ce63eb9c7c75b208
J <jordanmledford@gmail.com>
2016-12-09 18:09:35 -0500

    fixed schema of objects and added commenting

--- atbash-cipher.js
+++ atbash-cipher.js
@@ -1,40 +1,46 @@
 var atbash = function(string){
-  var object = {
-    A : Z
-  	B : Y
-    C : X
-    D : W
-    E : V
-    F : U
-    G : T
-  	H : S
-    I : R
-    J : Q
-    K : P
-    L : O
-    M : N
-    N : M
-    O : L
-    P : K
-    Q : J
-    R : I
-    S : H
-    T : G
-    U : F
-    V : E
-    W : D
-    X : C
-    Y : B
-    Z : A
-  }
-  var arrayOfCharacters = string.split()
-  console.log(arrayOfCharacters);
-  var encoder = function(character){
+  var schema = [
+    {'A' : 'Z'}
+  	{'B' : 'Y'}
+    {'C' : 'X'}
+    {'D' : 'W'}
+    {'E' : 'V'}
+    {'F' : 'U'}
+    {'G' : 'T'}
+  	{'H' : 'S'}
+    {'I' : 'R'}
+    {'J' : 'Q'}
+    {'K' : 'P'}
+    {'L' : 'O'}
+    {'M' : 'N'}
+    {'N' : 'M'}
+    {'O' : 'L'}
+    {'P' : 'K'}
+    {'Q' : 'J'}
+    {'R' : 'I'}
+    {'S' : 'H'}
+    {'T' : 'G'}
+    {'U' : 'F'}
+    {'V' : 'E'}
+    {'W' : 'D'}
+    {'X' : 'C'}
+    {'Y' : 'B'}
+    {'Z' : 'A'}
+]
 
+  var arrayOfCharacters = string.split()
+  var encoder = function(character){
+    character.toUpperCase()
+    // need to return opposite value in array
+    // and also return them each in objects.
   }
+
   var scrambler = function(array){
+    // for each character in array, return the associated value
+    // within the schema object
     return array.forEach(encoder)
   }
+
   var isASentence = function(){
     if(arrayofCharacters.indexOf(".") !== -1){
       return true
@@ -42,10 +48,13 @@ var atbash = function(string){
       return false
     }
   }
-  if (arrayOfCharacters.filter(isASentence)){
 
+  if (arrayOfCharacters.filter(isASentence)){
+    // do something sentence related
   } else {
     arrayofCharacters.scrambler()
   }
+  // need to add a space every 5 characters
   return arrayOfCharacters.join()
+
 }
